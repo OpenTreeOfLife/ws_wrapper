0fa3f5f655f468a80457154ea41d41a7f478bc2d
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-11-02 15:12:04 -0400

    Get /v3/tree_of_life/about working.

--- ws_wrapper/views.py
+++ ws_wrapper/views.py
@@ -1,7 +1,13 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 
+import requests
 
 @view_config(route_name='home')
-def my_view(request):
+def home_view(request):
     return Response('<body>This is home</body>')
+
+@view_config(route_name='tol:about')
+def tol_about_view(request):
+    r = requests.post("http://localhost:1984/v3/tree_of_life/about")
+    return Response(r.content)
