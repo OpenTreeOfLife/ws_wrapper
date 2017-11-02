cee2a62346e61cf398e36900ce073fe43769bf5a
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-11-02 16:17:45 -0400

    Add unused branch to replace tree1 argument with tree1newick argument.

--- ws_wrapper/views.py
+++ ws_wrapper/views.py
@@ -2,10 +2,16 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 import requests
+import json
 import peyotl
 
 # fixme - make the host and port that we are proxying for into variables in
 #         development.ini & production.ini
+
+# do we want to strip the outgroup?
+def newick_for_study_tree(study,tree)
+    print "study = {} tree = {}".format(study,tree)
+    return u"(ott1,ott2,(ott3,ott4));"
 
 @view_config(route_name='home')
 def home_view(request):
@@ -19,5 +25,13 @@ def tol_about_view(request):
 @view_config(route_name='conflict:conflict-status')
 def conflict_status_view(request):
     j = request.json_body
+    if 'tree1' in j.keys():
+        study_tree1 = j['tree1']
+        study1,tree1 = study_tree1.split('@')
+
+        if False:
+            j.pop('tree1',None)
+            j[u'tree1newick'] = newick_for_study_tree(study, tree)
+
     r = requests.post("http://localhost:1984/v3/conflict/conflict-status", json = j)
     return Response(r.content, r.status_code)
