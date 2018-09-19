cc4dcb134eecca8841c8138e4fc4f63c6c457848
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-09-19 14:06:57 -0400

    Send errors as JSON.

--- ws_wrapper/exceptions.py
+++ ws_wrapper/exceptions.py
@@ -1,8 +1,11 @@
 import logging
+import json
 log = logging.getLogger('ws_wrapper')
 
 class HttpResponseError(Exception):
     def __init__(self, body, code):
-        log.warn(body)
-        self.body = body
+        log.warn(body+"\n")
+        e = dict()
+        e["message"] = body
+        self.body = json.dumps(e,indent=4)+"\n"
         self.code = code
